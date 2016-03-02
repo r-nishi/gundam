bee8da30bbba206f4b90d11f7bded84cef159008
r-nishi <r-nishi@outlook.com>
2016-03-02 17:59:16 +0900

    first commit

---
+++ fuel/app/classes/controller/top.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     r-nishi
+ * @license    MIT License
+ * @copyright  2010 - 2015 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Top extends Controller
+{
+	/**
+	 * The basic welcome message
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		return Response::forge(View::forge('top/index'));
+	}
+
+	/**
+	 * A typical "Hello, Bob!" type example.  This uses a Presenter to
+	 * show how to use them.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_hello()
+	{
+		return Response::forge(Presenter::forge('welcome/hello'));
+	}
+
+	/**
+	 * The 404 action for the application.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(Presenter::forge('welcome/404'), 404);
+	}
+}
---
+++ fuel/app/logs/2016/03/01.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2016-03-01 11:41:01 --> Notice - Undefined property: Fuel\Core\Route::$method_param in /Applications/MAMP/htdocs/fuelphp-1.7.3/fuel/app/classes/controller/turnx.php on line 86
+ERROR - 2016-03-01 11:53:19 --> Notice - Undefined property: Fuel\Core\Route::$method_param in /Applications/MAMP/htdocs/fuelphp-1.7.3/fuel/app/classes/controller/turnx.php on line 86
