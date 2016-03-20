fc1e4d88bd359d63d2b0d4d65d5f2c01c2369257
r-nishi <r-nishi@outlook.com>
2016-03-21 06:55:57 +0900

    google console登録

--- fuel/app/views/exvsfb/head.php
+++ fuel/app/views/exvsfb/head.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="kq86V_frGPX2qFAuJ6hCRSOFl_k6kZLqcLvzbu03n-g" />
 <title><?php echo HP_NAME ?></title>
 
 <?php echo Asset::css('drawer.min.css') ?>
